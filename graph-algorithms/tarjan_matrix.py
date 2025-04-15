ebfeb8d371055b86d8ebfeb5cd4c6db8af055742
Bartosz <bartosz.drozdzynski@student.put.poznan.pl>
2025-04-15 20:13:22 +0200

    Create tarjan_matrix.py

---
+++ graph-algorithms/tarjan_matrix.py
@@ -0,0 +1,26 @@
+# W = WHITE - 0
+# G = GREY - 1 
+# B = BLACK - 2
+macierz = []
+kolor = []
+lista_L = []
+v = 0 #to jest wierzchołek który teraz będzie sprawdzany 
+# zmienianie kolorów
+def biali_sasiedzi(v):
+  global lista_L
+  i = 0 
+  kolor[v] = 1
+  while i < len(macierz[v]):
+    if macierz[v][i] == 1 and kolor[i] == 0:
+        kolor[i] = 1
+    i = i + 1
+  kolor[v] = 2
+  lista_L = [v] + lista_L
+koncowy = v
+while any(k==0 for k in kolor):
+  if kolor[v] == 0:
+      biali_sasiedzi(v)
+  v = v + 1
+  if v == len(macierz):
+      v = 0
+  
